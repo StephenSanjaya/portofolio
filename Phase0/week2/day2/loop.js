a38291c8a7776be72fb069ea95bb38791b306e40
StephenSanjaya <stephensanjaya5@gmail.com>
2024-01-31 11:58:37 +0700

    week2day2

---
+++ Phase0/week2/day2/loop.js
@@ -0,0 +1,51 @@
+// for loop:
+// for(var i = 0; i < 15; i++) {
+//     console.log(i);
+// }
+
+
+//foreach:
+// array = ["data1", "data2", "data3"]
+
+// array.forEach(function(element){
+//     console.log("halo" + element);
+// });
+// array.forEach(e => {
+//     console.log("halo", e);
+// });
+
+// repeat:
+// var name = "x";
+// console.log(name.repeat(10));
+
+// nested loop:
+// for (let i = 0; i < 5; i++) {
+//     for (let j = 10; j < 15; j++) {
+//         const element = array[index2];
+        
+//     }
+// }
+
+// while loop
+// let num = 2;
+// while (num <= 7) {
+//     console.log(num);
+//     num++;
+// }
+
+// do while
+// do {
+//     console.log(num);
+//     num++;
+// } while (num <= 7);
+
+let i = 1;
+while (i <= 20) {
+    console.log("Halo, saya orang ke " + i);
+    i++;
+}
+
+let j = 1;
+for (let j = 0; j <= 20; j++) {
+    console.log("Halo, saya orang ke " + j);
+}
