e7d964a820afd711256285089293fb33281c2079
StephenSanjaya <stephensanjaya5@gmail.com>
2024-02-05 10:57:26 +0700

    soal 9

---
+++ Phase0/week3/day1/preview-week2/soal9.js
@@ -0,0 +1,11 @@
+function cariPelaku(str) {
+    let arr = str.match(/a+b+c/g);
+
+    return arr.length;
+}
+
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
